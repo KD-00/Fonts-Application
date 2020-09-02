c593f6133598fad579b9b05117a8dad15f729c9f
Kuldeep <kuldeep258000@gmail.com>
2020-09-02 12:10:38 +0530

    App Showing Changed Font On Click

---
+++ app/src/main/java/com/example/fonts/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fonts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
+    private Button btnChangeFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHello = findViewById(R.id.txtHello);
+        btnChangeFont= findViewById(R.id.btnChangeFont);
+
+        final Typeface typeface = ResourcesCompat.getFont(this,R.font.maheisa_script);
+
+        btnChangeFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              txtHello.setTypeface(typeface);
+            }
+        });
+
+    }
+}
